3f6e71ff6879d5e53ef080673f0e37c08cad0370
whilealive <bruno.bischi@gmail.com>
2017-03-30 15:15:21 +0200

    testing

---
+++ all/.config/vis/visrc.lua
@@ -0,0 +1,21 @@
+-- ==================================================================
+-- FILE     .visrc.lua
+-- INFO     vis configuration file
+--
+-- DATE     30.03.2017
+-- OWNER    Bischofberger
+-- ==================================================================
+
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- per window configuration options
+  vis:command('set number')
+  vis:command('set cursorline')
+  vis:command('set escdelay 0')
+end)
