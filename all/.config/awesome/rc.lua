74e44706a180425f4dfa70801c932769b8ca9879
whilealive <bruno.bischi@gmail.com>
2021-05-02 22:03:15 +0200

    darktable and trash-cli installed

--- all/.config/awesome/rc.lua
+++ all/.config/awesome/rc.lua
@@ -2,7 +2,7 @@
 -- FILE     rc.lua
 -- INFO     awesome configuration file
 --
--- DATE     21.04.2021
+-- DATE     01.05.2021
 -- OWNER    Bischofberger
 -- ==================================================================
 
@@ -524,6 +524,7 @@ awful.rules.rules = {
     { rule = { class = "Inkscape" },      properties = { screen = 1, tag = "5:dtp", floating = true, maximized = false } },
     { rule = { class = "scribus" },       properties = { screen = 1, tag = "5:dtp", floating = true } },
     { rule = { class = "Xsane" },         properties = { screen = 1, tag = "5:dtp", floating = true } },
+    { rule = { class = "Darktable" },     properties = { screen = 1, tag = "5:dtp", floating = true } },
     -- 6:gui
     { rule = { class = "Thunar" },        properties = { screen = 1, tag = "6:gui", floating = true } },
     { rule = { class = "mpv" },           properties = { screen = 1, tag = "6:gui", floating = true } },
@@ -540,7 +541,6 @@ awful.rules.rules = {
     -- 8:virt
     { rule = { class = "VirtualBox" },    properties = { screen = 1, tag = "8:virt", floating = true } },
     -- currently unused
-    --{ rule = { class = "Darktable" },     properties = { screen = 1, tag = "5:dtp", floating = true } },
     --{ rule = { class = "Gnome-mplayer" }, properties = { screen = 1, tag = "6:gui", floating = true } },
     --{ rule = { class = "Chromium" },      properties = { screen = 1, tag = "7:web" } },
     --{ rule = { class = "Pavucontrol" },   properties = { screen = 1, tag = "6:gui", floating = true } },
